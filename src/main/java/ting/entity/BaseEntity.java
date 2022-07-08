1da431eb809d7cbd7bbfb4de31d2ef30f1a1f77a
Frederick-S <mao_xiaodan@hotmail.com>
2022-07-08 08:44:26 +0800

    entity: change id generation policy to idendity

--- src/main/java/ting/entity/BaseEntity.java
+++ src/main/java/ting/entity/BaseEntity.java
@@ -8,7 +8,7 @@ import javax.persistence.MappedSuperclass;
 @MappedSuperclass
 public class BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     public long getId() {
