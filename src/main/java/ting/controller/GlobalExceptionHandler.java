1ac16cc70396d33ecb0a8b6f567563b03e58a222
Frederick-S <mao_xiaodan@hotmail.com>
2022-07-10 11:53:33 +0800

    error handler: add global error handler

---
+++ src/main/java/ting/controller/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package ting.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import ting.dto.Error;
+import ting.dto.Response;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<Response<Void>> handleExceptions(Throwable e, WebRequest webRequest) {
+        return new ResponseEntity<>(new Response<>(new Error("Internal server error")), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
