4fe69aaa838a650b018eda2e038f542d847a8f44
Frederick-S <mao_xiaodan@hotmail.com>
2022-08-02 20:22:51 +0800

    create ting: upload audio file to azure blob storage

---
+++ src/main/java/ting/config/AzureBlobStorageConfig.java
@@ -0,0 +1,30 @@
+package ting.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "azure.storage")
+public class AzureBlobStorageConfig {
+    private String connectionString;
+
+    private String containerName;
+
+    public String getConnectionString() {
+        return connectionString;
+    }
+
+    public void setConnectionString(String connectionString) {
+        this.connectionString = connectionString;
+    }
+
+    public String getContainerName() {
+        return containerName;
+    }
+
+    public void setContainerName(String containerName) {
+        this.containerName = containerName;
+    }
+}
---
+++ src/main/java/ting/controller/BlobController.java
@@ -0,0 +1,44 @@
+package ting.controller;
+
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.common.sas.AccountSasPermission;
+import com.azure.storage.common.sas.AccountSasResourceType;
+import com.azure.storage.common.sas.AccountSasService;
+import com.azure.storage.common.sas.AccountSasSignatureValues;
+import com.azure.storage.common.sas.SasProtocol;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ting.annotation.LoginRequired;
+import ting.config.AzureBlobStorageConfig;
+
+import java.time.Duration;
+import java.time.OffsetDateTime;
+
+@RestController
+@RequestMapping("/blobs")
+public class BlobController extends BaseController {
+    @Autowired
+    private AzureBlobStorageConfig azureBlobStorageConfig;
+
+    @GetMapping("/sas")
+    @LoginRequired
+    public String sas() {
+        var blobServiceClient = new BlobServiceClientBuilder()
+                .connectionString(azureBlobStorageConfig.getConnectionString())
+                .buildClient();
+        AccountSasPermission accountSasPermission = new AccountSasPermission()
+                .setWritePermission(true);
+        AccountSasResourceType accountSasResourceType = new AccountSasResourceType()
+                .setObject(true);
+        AccountSasService accountSasService = new AccountSasService()
+                .setBlobAccess(true);
+        OffsetDateTime expiryTime = OffsetDateTime.now().plus(Duration.ofMinutes(5));
+        AccountSasSignatureValues accountSasSignatureValues = new AccountSasSignatureValues(expiryTime, accountSasPermission, accountSasService, accountSasResourceType)
+                .setProtocol(SasProtocol.HTTPS_HTTP);
+        String sas = blobServiceClient.generateAccountSas(accountSasSignatureValues);
+
+        return String.format("https://%s.blob.core.windows.net/%s?%s", blobServiceClient.getAccountName(), azureBlobStorageConfig.getContainerName(), sas);
+    }
+}
