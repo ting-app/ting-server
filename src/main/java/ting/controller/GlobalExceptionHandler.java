ac56c89b5a0c5f0c5b0a34a286f59aa71a5edb54
Frederick-S <mao_xiaodan@hotmail.com>
2022-08-09 13:31:45 +0800

    error handling: catch HttpMessageNotReadableException

--- src/main/java/ting/controller/GlobalExceptionHandler.java
+++ src/main/java/ting/controller/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package ting.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,6 +17,8 @@ import java.util.List;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ResponseError> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         BindingResult bindingResult = e.getBindingResult();
@@ -27,8 +32,15 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseError> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return new ResponseEntity<>(new ResponseError("HTTP 消息转换异常"), HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(Throwable.class)
     public ResponseEntity<ResponseError> handleRuntimeException(Throwable e) {
+        logger.error("Controller error", e);
+
         return new ResponseEntity<>(new ResponseError("服务器异常"), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
