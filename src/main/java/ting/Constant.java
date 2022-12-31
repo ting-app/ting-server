8a344e675e95f9591e07b025b928718d981a7a7d
Frederick-S <mao_xiaodan@hotmail.com>
2022-12-31 18:14:21 +0800

    default max page size

--- src/main/java/ting/Constant.java
+++ src/main/java/ting/Constant.java
@@ -5,5 +5,5 @@ package ting;
  */
 public class Constant {
     public static final String ME = "me";
-    public static final int MAX_PAGE_SIZE = 10;
+    public static final int MAX_PAGE_SIZE = 100;
 }
