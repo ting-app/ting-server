ef3156faa928b4478ea94931018b9a8c7667f434
Xiaodan Mao <xmao@zuora.com>
2023-01-21 16:30:53 +0800

    update init data

--- db/init.sql
+++ db/init.sql
@@ -105,3 +105,4 @@ CREATE TABLE `user` (
 
 insert into user (id, created_at, encrypted_password, name, email, verified) values (1, CURRENT_TIMESTAMP(6), '$2a$10$wJ0g3MsN5M/UogWIXkZHKuF.28jEsxJTuI61TXWxmng8T68FP2OYe', 'System', '', 1);
 insert into program (id, created_at, created_by, description, language, title, updated_at, visible) values (1, CURRENT_TIMESTAMP(6), 1, 'NHK NEWS WEB EASY 是 NHK 下面向小学生、中学生，以及在日本居住的外国人的简单新闻栏目。', 2, 'NHK NEWS WEB EASY', CURRENT_TIMESTAMP(6), 1);
+insert into program (id, created_at, created_by, description, language, title, updated_at, visible) values (2, CURRENT_TIMESTAMP(6), 1, 'VOA 慢速英语，语速比标准 VOA 慢三分之一。', 1, 'VOA 慢速英语', CURRENT_TIMESTAMP(6), 1);
