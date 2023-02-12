8af66125b79683e1636585633d1bcac0b3afec9d
Frederick-S <mao_xiaodan@hotmail.com>
2023-02-12 12:06:44 +0800

    unit test - create ting

--- src/test/java/ting/controller/TingControllerTest.java
+++ src/test/java/ting/controller/TingControllerTest.java
@@ -1,5 +1,6 @@
 package ting.controller;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -32,7 +33,7 @@ public class TingControllerTest extends BaseTest {
     }
 
     @Test
-    public void shouldReturn400WhenProgramDoesntBelongToCurrentUser() throws Exception {
+    public void shouldReturn401WhenProgramDoesntBelongToCurrentUser() throws Exception {
         Program program = createOtherUserProgram(1, true);
         TingDto tingDto = createTingDto(program.getId());
         String json = objectMapper.writeValueAsString(tingDto);
@@ -40,5 +41,54 @@ public class TingControllerTest extends BaseTest {
 
         mockMvc.perform(MockMvcRequestBuilders.post("/tings").contentType(MediaType.APPLICATION_JSON).content(json).cookie(cookies))
                 .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    public void shouldReturn400WhenCreateTing() throws Exception {
+        Program program = createMyProgram(1, true);
+        TingDto tingDto1 = new TingDto();
+        TingDto tingDto2 = new TingDto();
+        tingDto2.setProgramId(program.getId());
+        TingDto tingDto3 = new TingDto();
+        tingDto3.setProgramId(program.getId());
+        tingDto3.setTitle("title");
+        TingDto tingDto4 = new TingDto();
+        tingDto4.setProgramId(program.getId());
+        tingDto4.setTitle("title");
+        tingDto4.setDescription("description");
+        TingDto tingDto5 = new TingDto();
+        tingDto5.setProgramId(program.getId());
+        tingDto5.setTitle("title");
+        tingDto5.setDescription("description");
+        tingDto5.setAudioUrl("audio");
+
+        Cookie[] cookies = login();
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/tings").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(tingDto1)).cookie(cookies))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        mockMvc.perform(MockMvcRequestBuilders.post("/tings").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(tingDto2)).cookie(cookies))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        mockMvc.perform(MockMvcRequestBuilders.post("/tings").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(tingDto3)).cookie(cookies))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        mockMvc.perform(MockMvcRequestBuilders.post("/tings").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(tingDto4)).cookie(cookies))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        mockMvc.perform(MockMvcRequestBuilders.post("/tings").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(tingDto5)).cookie(cookies))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    public void shouldCreateTing() throws Exception {
+        Program program = createMyProgram(1, true);
+        TingDto tingDto = createTingDto(program.getId());
+
+        Cookie[] cookies = login();
+        String body = mockMvc.perform(MockMvcRequestBuilders.post("/tings").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(tingDto)).cookie(cookies))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        TingDto newTingDto = objectMapper.readValue(body, TingDto.class);
+        
+        Assertions.assertEquals(program.getId(), newTingDto.getProgramId());
     }
 }
